f06ac6728f2f8b39c04c774db8b5e9d10f4b1040
Iris <admin@irishost.xyz>
2019-02-25 07:56:41 -0700

    Init Upload

---
+++ Infinity/Main.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Infinity
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.bunifuSeparator1 = new Bunifu.Framework.UI.BunifuSeparator();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flatToggle1 = new FlatUI.FlatToggle();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.bunifuCircleProgressbar1 = new Bunifu.Framework.UI.BunifuCircleProgressbar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuSeparator1
+            // 
+            this.bunifuSeparator1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.bunifuSeparator1.LineThickness = 2;
+            this.bunifuSeparator1.Location = new System.Drawing.Point(-26, 111);
+            this.bunifuSeparator1.Name = "bunifuSeparator1";
+            this.bunifuSeparator1.Size = new System.Drawing.Size(639, 35);
+            this.bunifuSeparator1.TabIndex = 1;
+            this.bunifuSeparator1.Transparency = 255;
+            this.bunifuSeparator1.Vertical = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 29);
+            this.panel1.TabIndex = 2;
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.panel1;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(495, -8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGray;
+            this.label1.Location = new System.Drawing.Point(76, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(418, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Please accept our TOS to continue!";
+            // 
+            // flatToggle1
+            // 
+            this.flatToggle1.BackColor = System.Drawing.Color.Transparent;
+            this.flatToggle1.Checked = false;
+            this.flatToggle1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatToggle1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.flatToggle1.Location = new System.Drawing.Point(145, 213);
+            this.flatToggle1.Name = "flatToggle1";
+            this.flatToggle1.Options = FlatUI.FlatToggle._Options.Style3;
+            this.flatToggle1.Size = new System.Drawing.Size(76, 33);
+            this.flatToggle1.TabIndex = 5;
+            this.flatToggle1.Text = "flatToggle1";
+            this.flatToggle1.CheckedChanged += new FlatUI.FlatToggle.CheckedChangedEventHandler(this.flatToggle1_CheckedChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.DisabledLinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.ForeColor = System.Drawing.Color.DarkGray;
+            this.linkLabel1.LinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel1.Location = new System.Drawing.Point(-3, 221);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(144, 18);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "I accept the TOS:";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // bunifuCircleProgressbar1
+            // 
+            this.bunifuCircleProgressbar1.animated = false;
+            this.bunifuCircleProgressbar1.animationIterval = 5;
+            this.bunifuCircleProgressbar1.animationSpeed = 300;
+            this.bunifuCircleProgressbar1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuCircleProgressbar1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuCircleProgressbar1.BackgroundImage")));
+            this.bunifuCircleProgressbar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.bunifuCircleProgressbar1.ForeColor = System.Drawing.Color.DimGray;
+            this.bunifuCircleProgressbar1.LabelVisible = true;
+            this.bunifuCircleProgressbar1.LineProgressThickness = 8;
+            this.bunifuCircleProgressbar1.LineThickness = 5;
+            this.bunifuCircleProgressbar1.Location = new System.Drawing.Point(225, 178);
+            this.bunifuCircleProgressbar1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.bunifuCircleProgressbar1.MaxValue = 100;
+            this.bunifuCircleProgressbar1.Name = "bunifuCircleProgressbar1";
+            this.bunifuCircleProgressbar1.ProgressBackColor = System.Drawing.Color.DimGray;
+            this.bunifuCircleProgressbar1.ProgressColor = System.Drawing.Color.White;
+            this.bunifuCircleProgressbar1.Size = new System.Drawing.Size(121, 121);
+            this.bunifuCircleProgressbar1.TabIndex = 3;
+            this.bunifuCircleProgressbar1.Value = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::Infinity.Properties.Resources.INFINITY_sign22;
+            this.pictureBox1.Image = global::Infinity.Properties.Resources.infinity_loader;
+            this.pictureBox1.Location = new System.Drawing.Point(-107, -225);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(598, 353);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(44)))), ((int)(((byte)(47)))));
+            this.ClientSize = new System.Drawing.Size(570, 298);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.flatToggle1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bunifuCircleProgressbar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bunifuSeparator1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator1;
+        private System.Windows.Forms.Panel panel1;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.Button button1;
+        private Bunifu.Framework.UI.BunifuCircleProgressbar bunifuCircleProgressbar1;
+        private System.Windows.Forms.Label label1;
+        private FlatUI.FlatToggle flatToggle1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Infinity/Main.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Infinity
+{
+    public partial class Main : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        public static extern IntPtr CreateRoundRectRgn
+        (
+           int nLeftRect,     // x-coordinate of upper-left corner
+           int nTopRect,      // y-coordinate of upper-left corner
+           int nRightRect,    // x-coordinate of lower-right corner
+           int nBottomRect,   // y-coordinate of lower-right corner
+           int nWidthEllipse, // height of ellipse
+           int nHeightEllipse // width of ellipse
+        );
+
+        string Path = "c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity";
+
+        public Main()
+        {
+            InitializeComponent();
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Please accept our TOS to continue!";
+            label1.Font = new Font("Consolas", (float)15.75, FontStyle.Regular);
+
+
+            if (Directory.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity"))
+            {
+                if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION"))
+                {
+                    using (WebClient web = new WebClient())
+                    {
+                        string Version = web.DownloadString(new Uri("https://irishost.xyz/InfinityHosting/VERSION"));
+                        string CurrentVersion = File.ReadAllText("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION");
+                        //"c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity"
+                        if (CurrentVersion == Version)
+                        {
+                            Process.Start("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity" + "\\InfinityMain.exe");
+                            Task.Delay(250);
+                            Environment.Exit(0);
+                            return;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity");
+                if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION"))
+                {
+                    using (WebClient web = new WebClient())
+                    {
+                        string Version = web.DownloadString(new Uri("https://irishost.xyz/InfinityHosting/VERSION"));
+                        string CurrentVersion = File.ReadAllText("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION");
+                        //"c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity"
+                        if (CurrentVersion == Version)
+                        {
+                            Process.Start("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity" + "\\InfinityMain.exe");
+                            Task.Delay(250);
+                            Environment.Exit(0);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.irishost.xyz/#TOS");
+        }
+
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://clients3.google.com/generate_204"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private async void Init()
+        {
+            try
+            {
+                if (Process.GetProcessesByName("InfinityMain").Length != 0) for (var i = 0; i < Process.GetProcessesByName("InfinityMain").Length; i++) Process.GetProcessesByName("InfinityMain")[i].Kill(); Task.Delay(500);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error has a occured while closing old proccess's please try again!", "Yikes", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                Environment.Exit(0);
+                return;
+            }
+            try
+            {
+
+                if (Directory.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity"))
+                {
+                    if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\InfinityMain.exe") || File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\x86\\SQLite.Interop.dll") || File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\x86\\SQLite.Interop.dll") || File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\Update.zip"))
+                    {
+                        if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\InfinityMain.exe"))
+                        {
+                            File.Delete("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\InfinityMain.exe");
+                        }
+                        if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\x64\\SQLite.Interop.dll"))
+                        {
+                            File.Delete("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\x64\\SQLite.Interop.dll");
+                        }
+                        if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\x86\\SQLite.Interop.dll"))
+                        {
+                            File.Delete("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\x86\\SQLite.Interop.dll");
+                        }
+                        if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\Multiple_ROBLOX.exe"))
+                        {
+                            File.Delete("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\Multiple_ROBLOX.exe");
+                        }
+                        if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\Update.zip"))
+                        {
+                            File.Delete("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\Update.zip");
+                        }
+                        await Task.Delay(500);
+                    }
+                }
+                else
+                {
+                    Directory.CreateDirectory("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error has a occured while deleting the old files please try again!", "Yikes", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                Environment.Exit(0);
+            }
+        }
+
+        private void ProgChange(object sender, DownloadProgressChangedEventArgs e)
+        {
+            try
+            {
+                bunifuCircleProgressbar1.MaxValue = (int)e.TotalBytesToReceive;
+                bunifuCircleProgressbar1.Value = (int)e.BytesReceived;
+            }
+            catch { }
+        }
+
+        private async void Web_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            label1.Font = new Font("Consolas", (float)15.75, FontStyle.Regular);
+            label1.Location = new Point(106, 145);
+            label1.Text = "Download Complete, Launching!";
+
+            if (Process.GetProcessesByName("InfinityMain").Length != 0) for (var i = 0; i < Process.GetProcessesByName("InfinityMain").Length; i++) Process.GetProcessesByName("InfinityMain")[i].Kill();
+
+            ZipFile.ExtractToDirectory("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\Update.zip", "c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity");
+
+            
+
+            await Task.Delay(1000); Process.Start("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity" + "\\InfinityMain.exe");
+
+            if (File.Exists("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION"))
+            {
+                File.Delete("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION");
+                await Task.Delay(100);
+
+                File.Create("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION").Close();
+
+                await Task.Delay(250);
+                using (WebClient web = new WebClient())
+                {
+                    File.WriteAllText("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION", web.DownloadString("https://irishost.xyz/InfinityHosting/VERSION"));
+                }
+            }
+            else
+            {
+                using (WebClient web = new WebClient())
+                {
+                    File.WriteAllText("c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity\\__VERSION", web.DownloadString("https://irishost.xyz/InfinityHosting/VERSION"));
+                }
+            }
+            await Task.Delay(2000);
+            Application.Exit();
+        }
+
+        private void StartDownload()
+        {
+            if (CheckForInternetConnection())
+            {
+                Init();
+                using (WebClient web = new WebClient())
+                {
+                    web.DownloadFileAsync(new Uri(new WebClient().DownloadString("https://www.irishost.xyz/InfinityHosting/Update")), "c:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Infinity" + "\\Update.zip");
+                    web.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgChange);
+                    web.DownloadFileCompleted += new AsyncCompletedEventHandler(Web_DownloadFileCompleted);
+                }
+            }
+            else
+            {
+                DialogResult dialog = MessageBox.Show("This requries an internet connection!", "Yikes", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                Environment.Exit(0);
+            }
+        }
+
+
+
+        private void flatToggle1_CheckedChanged(object sender)
+        {
+            if (flatToggle1.Checked == true)
+            {
+                flatToggle1.Checked = false;
+                DialogResult dialog = MessageBox.Show("You have just agree'd to our terms you cannot undo now, would you like to continue?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+                if (dialog == DialogResult.Yes)
+                {
+                    label1.Text = "Thank you for accepting our TOS the download will start!";
+                    label1.Font = new Font("Consolas", 12, FontStyle.Regular);
+                    label1.Location = new Point(29, 149);
+                    flatToggle1.Enabled = false;
+                    flatToggle1.Checked = true;
+                    StartDownload();
+                }
+                else
+                {
+                    flatToggle1.Checked = false;
+                    label1.Text = "Please accept our TOS to continue!";
+                    label1.Font = new Font("Consolas", (float)15.75, FontStyle.Regular);
+                    label1.Location = new Point(76, 145);
+                }
+            }
+            else
+            {
+                label1.Text = "Please accept our TOS to continue!";
+                label1.Font = new Font("Consolas", (float)15.75, FontStyle.Regular);
+                label1.Location = new Point(76, 145);
+            }
+        }
+    }
+}
